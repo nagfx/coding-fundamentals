d3995a1115294f603759d2d2a6b6416931caace0
Naman <89750281+nagfx@users.noreply.github.com>
2022-09-14 16:01:41 +0500

    Create ComThinkAss3.js

---
+++ Modules/04-JS/Assigments/ComThinkAss3.js
@@ -0,0 +1,12 @@
+gens = {
+"-3": ["great grandfather", "great grandmother"],
+"-2": ["grandfather", "grandmother"],
+"-1": ["father", "mother"],
+"0": ["me!", "me!"],
+"1": ["son", "daughter"],
+"2": ["grandson", "granddaughter"],
+"3": ["great grandson", "great granddaughter"]
+}
+function generation(x,y) {
+return y=="m"?gens[x][0]:gen[x][1];
+}
